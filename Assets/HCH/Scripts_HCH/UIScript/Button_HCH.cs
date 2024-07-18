4c25feb55b392849d064f790af45e576c884153b
hch5362 <hch5362@gmail.com>
2024-07-18 09:04:04 +0900

    시작화면 제작(배경 이미지만 있으면 됨)

---
+++ Assets/HCH/Scripts_HCH/UIScript/Button_HCH.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+// 커서를 텍스트에 가져다 대면 색깔이 점점 하늘색으로 변하게 한다.
+// 커서를 떼면 다시 원 색깔로 변하게 한다.
+// r값 : 165
+
+public class Button_HCH : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    Text txt;
+    public Color originColor;
+    public Color pointerColor;
+
+    void Start()
+    {
+        txt = GetComponentInChildren<Text>();
+        txt.color = originColor;
+    }
+
+    // 커서 가져다 댔을 때 하늘색으로 변경
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        txt.color = pointerColor;
+    }
+
+    // 커서 떼면 원색으로 변경
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        txt.color = originColor;
+    }
+
+    // 게임 시작 버튼
+    // 현재 테스트용 씬 적용중
+    public void OnClickGameStart()
+    {
+        SceneManager.LoadScene("Main_HCH");
+    }
+
+    // 게임 종료 버튼
+    public void OnClickGameQuit()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
+    }
+}

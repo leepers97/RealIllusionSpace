2417f51a53aead03a2240c21007fa4bfa914eb79
hch5362 <hch5362@gmail.com>
2024-07-22 15:39:44 +0900

    사운드 추가 및 엔딩 오브젝트 제작

--- Assets/HCH/Scripts_HCH/GrabScript/SoundToScale_HCH.cs
+++ Assets/HCH/Scripts_HCH/GrabScript/SoundToScale_HCH.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 물체의 크기에 따라 다른 사운드를 출력하고 싶다
+// 스케일값 평균이 5이상이면 큰소리
+// 스케일값 평균이 1 ~ 5면 중간소리
+// 스케일값 평균이 1이하면 작은소리를 출력한다
+
 public class SoundToScale_HCH : MonoBehaviour
 {
+    float upper = 4f;
+    float lower = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +22,23 @@ public class SoundToScale_HCH : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) return;
+        float scaleAvr = (transform.localScale.x + transform.localScale.y + transform.localScale.z) / 3;
+        if(scaleAvr > upper)
+        {
+            SoundManager.instance.PlaySound("Col_Big", this.transform);
+        }
+        else if (scaleAvr < lower)
+        {
+            SoundManager.instance.PlaySound("Col_Small", this.transform);
+        }
+        else
+        {
+            SoundManager.instance.PlaySound("Col_Middle", this.transform);
+        }
     }
 }

af8a4f9e0623319a65fd624c590843910996c1f7
hch5362 <hch5362@gmail.com>
2024-07-29 17:38:03 +0900

    맵제작 3일차

---
+++ Assets/HCH/Scripts_HCH/ColliderScript/Teleport_HCH.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 콜라이더를 지나가면 지정한 스폰 포인트로 텔레포트한다
+
+public class Teleport_HCH : MonoBehaviour
+{
+    public Transform spawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("11");
+        if (other.CompareTag("Player"))
+        {
+            print("22");
+            other.transform.position = spawnPoint.position;
+        }
+    }
+}
